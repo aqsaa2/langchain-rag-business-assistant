a1eca036271ebe3407edbfef2dd7c786412914c3
Aqsa Asif <132770829+aqsaa2@users.noreply.github.com>
2025-01-27 07:09:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import time
 import uuid
 import logging
 import pandas as pd
-import pysqlite3
-import sys
-import chromadb
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.schema import HumanMessage, AIMessage
 from langchain_community.vectorstores.chroma import Chroma
@@ -21,9 +18,12 @@ from test_app import (
     get_stored_data,
     get_user_responses_from_db,
 )
+import chromadb
 from chromadb import PersistentClient
-
+import pysqlite3
+import sys
 sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
